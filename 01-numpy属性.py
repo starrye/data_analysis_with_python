24cbf9a672bedf53d4b771ab282c295c9112c6e9
lwz <hello_lwz@163.com>
2018-10-29 17:13:44 +0800

    01-numpy属性

---
+++ 01-numpy属性.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+array = np.array([[1,2,3],[4,5,6]])
+print(array)
+#查看数组维数
+print('number of dim:',array.ndim)
+#查看数组元素个数
+print('size:',array.size)
+#查看数组行数 列数 返回（）
+print('shape:',array.shape)
