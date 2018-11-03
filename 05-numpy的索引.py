cc7ce14f48609902f898a0ce2d167d344faec690
lwz <hello_lwz@163.com>
2018-11-03 10:33:17 +0800

    05-numpy的索引

---
+++ 05-numpy的索引.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+a = np.arange(3,15).reshape((3,4))
+print(a[0]) # 打印第0行
+print(a[0][0]) # [x][y]打印第x行第y列
+print(a[0,0])  #打印[x,y]第x行第y列
+print(a[0,:])  # 打印[x,:]第x行
+print(a[:,0]) #打印[:,x]第x列
+print(a[1,1:3]) #打印[x,y:z] 第x行 y列到z列（不包括z列）的值
+
+print('-----')
+#for 循环打印每行
+for row in a:
+    print(row)
+print('-----')
+#for 循环打印每列
+for column in a.T:
+    print(column)
+
+print(a.flatten()) #矩阵转列表
+# for item in a.flatten():  #打印每一个元素
+for item in a.flat: #打印每一个元素
+    print(item)
